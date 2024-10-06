a42d4aedb84cd756716ccf8c661dde9341f4b468
Dmytro Potapchuk <traiget@gmail.com>
2024-10-06 12:13:10 +0300

    change base url

--- src/http_common.js
+++ src/http_common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "https://newsapi.org/v2",
+    baseURL: "https://localhost:5000",
     headers: {
         "Content-Type": "application/json"
     }
